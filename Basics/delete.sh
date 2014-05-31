bba6acab4801c66783fc49ec1461fc2e2d509920
Sangeetha Busangari <sangeetha.785@gmail.com>
2014-05-31 22:57:58 +0530

    delete if the second file contents are same.

---
+++ Basics/delete.sh
@@ -0,0 +1,16 @@
+if(($#==2)) then
+if [[-f $1 && -f $2]] then
+  if comp $1 $2 ?/dev/null then
+    print "$1 and $2 contents are same"
+    print "$2 is removing.."
+    rm $2
+    print "$2 is deleted"
+  else
+    print "$1 and $2 are different"
+  fi
+  else
+    print "Two arguments should be ordinary files"
+  fi
+  else
+  print "enter exactly 1 argumants"
+fi
